e0342bf63be09f4bc6226ca19322a4bf8befa70c
HaolinPiolin <89527193+HaolinPiolin@users.noreply.github.com>
2021-09-25 21:17:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
 self.addEventListener("install", e => {
-    console.log("Install!");
+    e.waitUntill(
+      caches.open("static").then(cache => {
+        return cache.addAll(["./", "css.css", "./favicon.png"]);
+      })
+    );
 });
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
+
