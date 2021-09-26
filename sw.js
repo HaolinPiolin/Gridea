7762c11f02c51b53a1b9e206eef844b26ccedd8c
HaolinPiolin <89527193+HaolinPiolin@users.noreply.github.com>
2021-09-26 14:21:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,6 +12,19 @@ window.addEventListener('beforeinstallprompt', (e) => {
   console.log(`'beforeinstallprompt' event was fired.`);
 });
 
+buttonInstall.addEventListener('click', async () => {
+  // Hide the app provided install promotion
+  hideInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with outcome of user choice
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We've used the prompt, and can't use it again, throw it away
+  deferredPrompt = null;
+});
+
 self.addEventListener("install", e => {
     e.waitUntil(
       caches.open("static").then(cache => {
